3f1f909997de2563424f153b7ed106b40e2ba50a
batamsingh <batampuyam09@gmail.com>
2024-11-08 00:04:02 +0530

    setting up landing page

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,17 +1,73 @@
-import React from 'react'
+import React from "react";
 
-const Card = ({heading, details}) => {
+const Card = ({ heading, details }) => {
   return (
-    <div className='w-80 h-80 shadow-md flex flex-col bg-white'>
-        <div className='h-4/5 w-full p-10 text-center'>
-            <h2 className='font-bold text-xl mt-5'>{heading && heading}</h2>
-            <p className='mt-8 text-gray-500'>{details && details}</p>
+    <div className="w-80 h-80 shadow-md flex flex-col bg-white">
+      <div className="h-4/5 w-full p-10 text-center">
+        <h2 className="font-bold text-xl mt-5">{heading && heading}</h2>
+        <p className="mt-8 text-gray-500">{details && details}</p>
+      </div>
+      <div className="font-bold bg-slate-200 flex-1 flex justify-center items-center">
+        <button className="hover:text-red-600 transition duration-300">
+          Read More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+
+export const Card2 = ({ img, title, description, logo }) => {
+  return (
+    <div className="flex flex-col justify-center items-center group hover:cursor-pointer">
+      <div className="h-64 w-80 overflow-clip">
+        <img className="group-hover:scale-125 transition-all ease-in-out duration-300 group-hover:brightness-50" src={img} alt="img" />
+      </div>
+      <div className="-mt-20 relative flex items-center bg-white h-56 w-64">
+        <div className="bg-slate-200 h-full">{logo}</div>
+        <div className="p-5">
+          <h1 className="text-black text-2xl font-semibold mb-2">{title}</h1>
+          <p className="text-slate-800">{description}</p>
         </div>
-        <div className='font-bold bg-slate-200 flex-1 flex justify-center items-center'>
-            <button className='hover:text-red-600 transition duration-300'>Read More</button>
+        <div
+          className="w-14 h-14 rounded-full bg-customRed absolute -top-7 right-7 flex justify-center items-center 
+         group-hover:h-28 group-hover:rounded-t-full group-hover:rounded-b-none group-hover:-top-14 
+        transition-all duration-200 ease-in-out"
+        >
+          <svg
+            width="35px"
+            height="35px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 12H20M12 4V20"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </div>
+      </div>
+    </div>
+  );
+};
+
+export const ProcessCard = ({ sl, title, description }) => {
+  return (
+    <div className="flex flex-col h-64 w-72 items-center text-center border-2 border-t-customRed relative hover:-mt-4 transition-all ease-in-out duration-300 group">
+      <div className="bg-white rounded-full w-16 h-16 border-2 border-customRed -mt-8 absolute flex items-center justify-center group-hover:bg-customRed group-hover:text-white transition-all ease-out duration-300">
+        <h2 className="font-semibold text-2xl">{sl}</h2>
+      </div>
+
+      <div className="my-14 px-5">
+        <h2 className="text-2xl font-semibold">{title}</h2>
+        <p className="text-slate-700 mt-5">{description}</p>
+      </div>
+
     </div>
   )
 }
-
-export default Card
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import img4 from "../assets/images/kitchen1.jpg";
 import img5 from "../assets/images/kitchen2.jpg";
 import img6 from "../assets/images/kitchen3.jpg";
 import img7 from "../assets/images/kitchen4.jpg";
-import Card from "../component/Card";
+import Card, { Card2, ProcessCard } from "../component/Card";
 
 const images = [img1, img2, img3, img4, img5, img6, img7];
 
@@ -23,6 +23,30 @@ const Home = () => {
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
+
+  const coveredIndustries = [
+    {
+      img: img1,
+      title: "Residential",
+      description:
+        "Indignation and dislike men who are so beguiled and our the charms moment.",
+      logo: "logo1",
+    },
+    {
+      img: img2,
+      title: "Commercial",
+      description:
+        "Righteous indignation work are so beguiled demoralized the blinded by desire.",
+      logo: "logo2",
+    },
+    {
+      img: img3,
+      title: "Industries",
+      description:
+        "Our power of choice is and when nothing prevents work every pleasure interior.",
+      logo: "logo3",
+    },
+  ];
 
   // auto-slide effect
   useEffect(() => {
@@ -76,25 +100,109 @@ const Home = () => {
 
         {/* about company here */}
         <div className="flex justify-center gap-20 sm:flex-col md:flex-row mt-56">
-          <div className="w-[500px] h-[500px] border">
-
-          </div>
+          <div className="w-[500px] h-[500px] border"></div>
 
           <div className="w-1/4">
             <h3 className="font-bold text-red-500">ABOUT COMPANY</h3>
-            <h1 className="font-semibold text-3xl">A SMALL EFFICIENT INTERIOR DESIGNING TEAM</h1>
-            <p className="mt-20">We denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound.
-
-Indignation and dislike men who are so beguiled and demoralized by the charms pleasure of the moment, so blinded by desire, that they cannot foresee.</p>
+            <h1 className="font-semibold text-3xl">
+              A SMALL EFFICIENT INTERIOR DESIGNING TEAM
+            </h1>
+            <p className="mt-20">
+              We denounce with righteous indignation and dislike men who are so
+              beguiled and demoralized by the charms of pleasure of the moment,
+              so blinded by desire, that they cannot foresee the pain and
+              trouble that are bound. Indignation and dislike men who are so
+              beguiled and demoralized by the charms pleasure of the moment, so
+              blinded by desire, that they cannot foresee.
+            </p>
           </div>
         </div>
+
+        {/* covered areas */}
+        <div className="mt-32 min-h-[600px] flex flex-col items-center py-20 w-full bg-gray-800 opacity-80">
+          <div className="text-center">
+            <h6 className="text-md font-bold text-red-500">Working Area</h6>
+            <h1 className="text-white text-3xl m-5">
+              <strong>COVERED</strong> INDUSTRIES
+            </h1>
+            <p className="text-slate-300 text-lg">
+              We are an Interior Designer, Who believe in excellence, quality
+              and honesty, yes we<br></br> design beautiful home interiors.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-3 gap-10 my-10">
+            {coveredIndustries && coveredIndustries.length > 0
+              ? coveredIndustries.map((item) => (
+                  <Card2
+                    img={item.img}
+                    title={item.title}
+                    description={item.description}
+                    logo={item.logo}
+                  />
+                ))
+              : ""}
+          </div>
+        </div>
+
+        {/* completed projects */}
+        <div className="flex flex-col items-center my-20">
+
+          <div className="flex justify-between px-5 sm:w-full md:w-full lg:w-1/2">
+            <div>
+              <h6 className="text-customRed font-semibold my-5">PROJECTS</h6>
+              <h1 className="text-3xl">
+                <strong>RECENTLY COMPLETED</strong> WORKS
+              </h1>
+            </div>
+            <button className="font-bold hover:text-customRed">More Projects</button>
+          </div>
+
+          <div className="w-full grid grid-cols-5 gap-5 px-5 my-10">
+
+            <img src={img1} />
+            <img src={img2} />
+            <img src={img3} />
+            <img src={img4} />
+            <img src={img6} />
+              
+          </div>
+
+
+        </div>
+
+        {/* our working process */}
+        <div className="flex flex-col items-center">
+          <div>
+            <h6 className="text-customRed font-semibold">TO DO GOOD DESIGN</h6>
+            <h1 className="text-3xl"><strong>OUR WORKING</strong> PROCESS</h1>
+          </div>
+
+          <div className="flex justify-center gap-10 py-20">
+              <ProcessCard 
+              sl="01"
+              title="Idea & Design"
+              description="With righteous indignation and works off beguiled demoralized charm."
+              />
+              <ProcessCard 
+              sl="02"
+              title="Specification"
+              description="Our power of choice is untrammelled and when nothing prevents."
+              />
+              <ProcessCard 
+              sl="03"
+              title="Execution"
+              description="Wing to the claims of duty the obligations will frequently occur."
+              />
+          </div>
+
+        </div>
+
 
 
 
       </div>
       {/* body end */}
-
-
     </div>
   );
 };
